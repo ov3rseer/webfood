200180b24ca5230aba6110002fd55ad1d1138b5e
SergeyDashko <aramaz24@gmail.com>
2019-07-04 14:39:57 +0300

    master

--- backend/tests/_support/UnitTester.php
+++ backend/tests/_support/UnitTester.php
@@ -1,6 +1,8 @@
 <?php
 namespace backend\tests;
 
+use Codeception\Actor;
+
 /**
  * Inherited Methods
  * @method void wantToTest($text)
@@ -16,7 +18,7 @@ namespace backend\tests;
  *
  * @SuppressWarnings(PHPMD)
  */
-class UnitTester extends \Codeception\Actor
+class UnitTester extends Actor
 {
     use _generated\UnitTesterActions;
    /**
