0434c47f2c1c5a6fda579304d967dc9655c9e3d7
SergeyDashko <aramaz24@gmail.com>
2019-07-25 16:41:56 +0300

    Правки User

--- common/models/reference/User.php
+++ common/models/reference/User.php
@@ -27,7 +27,7 @@ class User extends Reference implements IdentityInterface
     /**
      * @var string
      */
-    protected $_password = 'VERY_BAD_CUSTOMER';
+    protected $_password;
 
     /**
      * @inheritdoc
