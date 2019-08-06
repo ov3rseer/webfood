a682c78b7add11915b81e2d3861a04c5debb7a4b
SergeyDashko <aramaz24@gmail.com>
2019-08-06 11:27:24 +0300

    Добавление администратора из справочника

--- common/models/reference/User.php
+++ common/models/reference/User.php
@@ -316,15 +316,20 @@ class User extends Reference implements IdentityInterface
             if($this->password){
                 $this->setPassword($this->password);
             }
-            if($this->user_type_id == UserType::ADMIN){
-                // Добавляем роль суперадмина
-                $auth = Yii::$app->authManager;
-                $role = $auth->getRole('super-admin');
-                // Добавляем админа
-                $auth->assign($role, $this->id);
-            }
         }
         return $parentResult;
+    }
+
+    public function afterSave($insert, $changedAttributes)
+    {
+        parent::afterSave($insert, $changedAttributes);
+        if($this->user_type_id == UserType::ADMIN){
+            // Добавляем роль суперадмина
+            $auth = Yii::$app->authManager;
+            $role = $auth->getRole('super-admin');
+            // Добавляем админа
+            $auth->assign($role, $this->id);
+        }
     }
 
     /**
