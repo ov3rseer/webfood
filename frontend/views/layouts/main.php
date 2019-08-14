39e2d48f651d04571e548616cc0dbb6add247835
alvastudio <alvastd@gmail.com>
2019-08-14 12:09:11 +0300

    bootstrap 4

--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -4,9 +4,10 @@
 /* @var $content string */
 /* @throws \Exception */
 
+use yii\bootstrap4\ActiveForm;
 use yii\helpers\Html;
-use yii\bootstrap\Nav;
-use yii\bootstrap\NavBar;
+use yii\bootstrap4\Nav;
+use yii\bootstrap4\NavBar;
 use yii\widgets\Breadcrumbs;
 use frontend\assets\AppAsset;
 use common\widgets\Alert;
