9ab0d32cfea676ca3129ad8a1652be7a5bee1341
SergeyDashko <aramaz24@gmail.com>
2019-08-16 14:27:28 +0300

    Terminal

--- terminal/controllers/TerminalModelController.php
+++ terminal/controllers/TerminalModelController.php
@@ -2,11 +2,120 @@
 
 namespace terminal\controllers;
 
+use common\helpers\ArrayHelper;
+use common\models\ActiveRecord;
+use frontend\models\FrontendForm;
 use Yii;
+use yii\base\InvalidConfigException;
+use yii\filters\AccessControl;
+use yii\filters\VerbFilter;
 use yii\web\Controller;
+use yii\web\NotFoundHttpException;
+use yii\web\Response;
 
 class TerminalModelController extends Controller
 {
+    /**
+     * @var string имя класса модели
+     */
+    public $modelClass;
+
+    /**
+     * @inheritdoc
+     * @throws InvalidConfigException
+     */
+    public function init()
+    {
+        parent::init();
+        if ($this->modelClass === null) {
+            throw new InvalidConfigException(get_class($this) . '::$modelClass не указан.');
+        }
+        if (!is_subclass_of($this->modelClass, ActiveRecord::class, true) &&
+            !is_subclass_of($this->modelClass, FrontendForm::class, true)) {
+            throw new InvalidConfigException(get_class($this) . '::$modelClass не является подклассом ' .
+                ActiveRecord::class . '.');
+        }
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function actions()
+    {
+        return array_merge(parent::actions(), [
+            'index' => [
+                'class' => 'terminal\actions\base\IndexAction',
+                'modelClass' => $this->modelClass,
+                'viewPath' => '@terminal/views/base/index',
+            ],
+        ]);
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function behaviors()
+    {
+        return ArrayHelper::merge(parent::behaviors(), [
+            'access' => [
+                'class' => AccessControl::class,
+                'rules' => [
+                    [
+                        'actions' => ['index'],
+                        'allow' => true,
+                        'roles' => ['@'],
+                    ],
+                ],
+            ],
+            'verbs' => [
+                'class' => VerbFilter::class,
+                'actions' => [
+                    'delete' => ['POST'],
+                    'delete-checked' => ['POST'],
+                ],
+            ],
+        ]);
+    }
+
+    /**
+     * Создание новой модели
+     * @param string|null $modelClass
+     * @return ActiveRecord
+     */
+    public function createModel($modelClass = null)
+    {
+        /* @var ActiveRecord $modelClass */
+        $modelClass = $modelClass ? $modelClass : $this->modelClass;
+        return new $modelClass();
+    }
+
+    /**
+     * Поиск модели по ее ID
+     * @param string|integer $id
+     * @param string|null $modelClass
+     * @return ActiveRecord
+     * @throws NotFoundHttpException
+     */
+    public function findModel($id, $modelClass = null)
+    {
+        /* @var ActiveRecord $modelClass */
+        $modelClass = $modelClass ? $modelClass : $this->modelClass;
+        $keys = $modelClass::primaryKey();
+        if (count($keys) > 1) {
+            $values = explode(',', $id);
+            if (count($keys) === count($values)) {
+                $model = $modelClass::findOne(array_combine($keys, $values));
+            }
+        } elseif ($id !== null) {
+            $model = $modelClass::findOne($id);
+        }
+        if (isset($model)) {
+            return $model;
+        } else {
+            throw new NotFoundHttpException('Элемент с указанным ID не найден: ' . $id);
+        }
+    }
+
     /**
      * Вывод представления в разных шаблонах в зависимости от типа запроса
      * @param string $view
@@ -24,5 +133,22 @@ class TerminalModelController extends Controller
             }
             return $this->render($view, $params);
         }
+    }
+
+    /**
+     * Перенаправление с учетом настроек, заданных в $_GET
+     * @param array|string $url
+     * @param int $statusCode
+     * @return Response
+     */
+    public function autoRedirect($url, $statusCode = 302)
+    {
+        $newUrl = Yii::$app->request->get('redirect', false);
+        if ($newUrl) {
+            $url = $newUrl;
+        } else if (is_array($url) && ($layout = Yii::$app->request->get('layout', false))) {
+            $url['layout'] = $layout;
+        }
+        return $this->redirect($url, $statusCode);
     }
 }
