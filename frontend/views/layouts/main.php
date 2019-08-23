8f469508cf58b0027a02e21cb06bba0fc0f9755d
wow64cpu <aleksandr19982012@gmail.com>
2019-08-23 11:44:19 +0300

    edits

--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -125,7 +125,7 @@ $().ready(function() {
         </div>
     </nav>
 
-    <div class="container-fluid">
+    <div class="container">
         <?= Breadcrumbs::widget(['links' => isset($this->params['breadcrumbs']) ? $this->params['breadcrumbs'] : []]) ?>
         <?= Alert::widget() ?>
         <?= $content ?>
