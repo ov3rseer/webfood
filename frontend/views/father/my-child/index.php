868880da706c6f3f59e53cbf5261d0a20e3ad4f1
SergeyDashko <aramaz24@gmail.com>
2019-11-19 13:44:36 +0300

    Фиксы

--- frontend/views/father/my-child/index.php
+++ frontend/views/father/my-child/index.php
@@ -166,7 +166,7 @@ $this->registerJs("
         }          
         var childId = $(this).data('child-id');
         $.ajax({
-            url: " . Url::to(['father/my-child/delete-child']) . ",
+            url: '" . Url::to(['father/my-child/delete-child']) . "',
             data: {'childId' : childId},
             dataType: 'json',
             type: 'POST', 
