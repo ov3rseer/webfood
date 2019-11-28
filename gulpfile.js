76c40c76223898f3b649302e6229f06097cdd685
SergeyDashko <aramaz24@gmail.com>
2019-11-28 14:14:53 +0300

    Правки

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,14 @@
 // Include gulp
 let gulp = require('gulp');
 let sass = require('gulp-sass');
-let rename = require('gulp-rename');
 // Include Our Plugins
 let jshint = require('gulp-jshint');
 let concat = require('gulp-concat');
 let terser = require('gulp-terser');
 // var sourcemaps = require('gulp-sourcemaps');
-
 let del = require('del'); // Подключаем библиотеку для удаления файлов и папок
 let autoprefixer = require('gulp-autoprefixer');// Подключаем библиотеку для автоматического добавления префиксов
-let combineMq = require('gulp-combine-mq');
 let multiDest = require('gulp-multi-dest');
-
 const args = require('yargs').argv;
 
 //var streamCombiner = require('stream-combiner');
@@ -20,18 +16,22 @@ const args = require('yargs').argv;
 // https://github.com/gulpjs/vinyl-fs/issues/67
 // https://www.npmjs.com/package/stream-combiner
 
-let frontendWebDir = 'frontend';
-let backendWebDir = 'backend';
-let terminalWebDir = 'terminal';
-
 let build = {
-    'dist': {
+    'frontend': {
         'build': true,
-        'path': '/web/'
+        'path': 'frontend/web/'
+    },
+    'backend': {
+        'build': true,
+        'path': 'backend/web/'
+    },
+    'terminal': {
+        'build': true,
+        'path': 'terminal/web/'
     }
 };
 
-function foldersNamesTwoFullPaths(webDir, folder) { // принимает название папки (строку или массив з нескольких и на основе build[] возвращает массив полных путей к папкам
+function foldersNamesTwoFullPaths(folder) { // принимает название папки (строку или массив з нескольких и на основе build[] возвращает массив полных путей к папкам
 
     let fullPaths = [];
     let folderNames = [];
@@ -43,181 +43,80 @@ function foldersNamesTwoFullPaths(webDir, folder) { // принимает наз
     } else if (typeof (folder) == 'string' && folder !== '') {
         folderNames = [folder];
     }
-
-    for (let key in build) {
-        if (build[key]['build']) {
-            for (let i = folderNames.length - 1; i >= 0; i--) {
-                let path = webDir + build[key]['path'] + '/' + folderNames[i];
-                fullPaths.push(path);
-            }
+    let webDir = args.path;
+    if (build[webDir]['build']) {
+        for (let i = folderNames.length - 1; i >= 0; i--) {
+            let path = build[webDir]['path'] + '/' + folderNames[i];
+            fullPaths.push(path);
         }
     }
+
 
     return fullPaths; // массив с путями
 }
 
-gulp.task('clean', function () { // Удаляем папку dist/assets перед сборкой
-    let actions = {
-        'clean': ['css', 'js', 'img'],
-        'clean-img': ['img'],
-        'clean-css': ['css'],
-        'clean-js': ['js'],
-    };
-    let webDir = args.path;
-    if (webDir) {
-        return del(foldersNamesTwoFullPaths(webDir, 'assets'), {force: true});
-    } else {
-        throw 'ошибк';
-    }
+gulp.task('clean', function () {
+    return del(foldersNamesTwoFullPaths(['css', 'js', 'img']), {force: true});
+});
+
+gulp.task('clean-js', function () {
+    return del(foldersNamesTwoFullPaths(['js']), {force: true});
+});
+
+gulp.task('clean-css', function () {
+    return del(foldersNamesTwoFullPaths(['css']), {force: true});
+});
+
+gulp.task('clean-img', function () {
+    return del(foldersNamesTwoFullPaths(['img']), {force: true});
 });
 
 // Lint Task
 gulp.task('lint', function () {
-    return gulp.src('src/js/**/*.js')
+    let webDir = args.path;
+    return gulp.src(build[webDir]['path'] + 'source/js/**/*.js')
         .pipe(jshint())
         .pipe(jshint.reporter('default'));
 });
 
 // Compile Our Sass
 gulp.task('sass', function () {
-    console.log(foldersNamesTwoFullPaths('css'));
-    return gulp.src('src/scss/**/*.scss')
-    // .pipe(sourcemaps.init()) //
+    let webDir = args.path;
+    return gulp.src(build[webDir]['path'] + 'source/scss/**/*.scss')
         .pipe(sass())
-        // .pipe(sourcemaps.write()) //
         .pipe(autoprefixer(['last 15 versions', '> 1%', 'safari 5', 'ie7', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'], {cascade: true})) // Создаем префиксы
         .pipe(multiDest(foldersNamesTwoFullPaths('css')));
-    // .pipe(gulp.dest('dist/assets/css'));
 });
-
 
 // Concatenate & Minify JS
 gulp.task('scripts', function () {
-    return gulp.src([
-        'src/js/**/*.js'
-    ])
-        .pipe(concat('all.js'))
-        // .pipe(gulp.dest('dist/assets/js'))
-        .pipe(rename('all.min.js'))
+    let webDir = args.path;
+    return gulp.src([build[webDir]['path'] + 'source/js/**/*.js'])
+        .pipe(concat('main.js'))
         .pipe(terser())
         .pipe(multiDest(foldersNamesTwoFullPaths('js')));
 });
 
-// Compile Our Sass
-gulp.task('fontawesome', function () {
-    console.log(foldersNamesTwoFullPaths('webfonts'));
-    return gulp.src('node_modules/@fortawesome/fontawesome-free/webfonts/*') // Переносим шрифты в продакшен
-        .pipe(multiDest(foldersNamesTwoFullPaths('webfonts')));
-    // .pipe(gulp.dest('dist/assets/webfonts'));
-});
-
 // Переносим картинки в продакшен
 gulp.task('img', function () {
-
-    return gulp.src([
-        'src/img/**/*',
-        '!src/img/**/__**',
-        '!src/img/**/__**/**/*',
-    ])
+    let webDir = args.path;
+    return gulp.src([build[webDir]['path'] + 'source/img/**/*',])
         .pipe(multiDest(foldersNamesTwoFullPaths('img')));
 });
 
 // Watch Files For Changes
 gulp.task('watch', function () {
-    // gulp.watch('src/js/*.js', ['lint', 'scripts']);
-    // gulp.watch('src/scss/*.scss', ['scss']);
-    gulp.watch('src/scss/**/*.scss', gulp.series('clean-css', 'sass'));
-    gulp.watch('src/js/**/*.js', gulp.series('lint', 'clean-js', 'scripts'));
-    gulp.watch([
-        'src/img/**/*',
-        '!src/img/**/__**',
-        '!src/img/**/__**/**/*',
-    ], gulp.series('clean-img', 'img'));
+    let webDir = args.path;
+    if (webDir) {
+        gulp.watch(build[webDir]['path'] + 'source/scss/**/*.scss', gulp.series('clean-css', 'sass'));
+        gulp.watch(build[webDir]['path'] + 'source/js/**/*.js', gulp.series('lint', 'clean-js', 'scripts'));
+        gulp.watch(build[webDir]['path'] + 'source/img/**/*', gulp.series('clean-img', 'img'));
+    } else {
+        throw 'Ошибка. Не указан параметр --path [terminal, backend, frontend]';
+    }
 });
 
-gulp.task('build', gulp.series(
-    'clean',
-    gulp.parallel(
-        // 'fontawesome',
-        'img',
-        'sass',
-        'scripts'
-    )
-));
-
-// Default Task
-gulp.task('default', gulp.series(
-    'lint',
-    'build',
-    'watch'
-));
-
-//
-// var minifyCss = require('gulp-clean-css');//минификация css
-// var notify = require('gulp-notify');
-// var clean = require('gulp-clean');
-//
-// //==============================================================================
-// //**************************  FrontEnd  ****************************************
-// //==============================================================================
-// //ПУТИ
-// var F_webDir = 'frontend/web/';
-//
-// var F_sourseDir = F_webDir + 'source/',
-//     F_sassDir = F_sourseDir + 'sass/',
-//     F_sassMainFile = F_sassDir + 'main.scss';
-//
-// var F_destCssDir = F_webDir + 'styles/css/',
-//     F_destCssMinDir = F_webDir + 'styles/css-min/';
-//
-// var sassOptions = {
-//     outputStyle: 'nested',
-//     precison: 3,
-//     errLogToConsole: true,
-// };
-//
-// //------------------------------------------------------------------------------
-// //                  компиляция sass
-// //------------------------------------------------------------------------------
-// gulp.task('front:compileSass', ['front:clean'], function () {
-//     return gulp
-//         .src([F_sassMainFile])
-//         .pipe(sass(sassOptions).on('error', sass.logError))
-//         .pipe(autoprefixer({
-//             browsers: ['last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'],
-//             cascade: false
-//         }))
-//         .pipe(combineMq({
-//             beautify: false
-//         }))
-//         .pipe(gulp.dest(F_destCssDir))
-//         .pipe(rename({suffix: '.min'}))
-//         .pipe(minifyCss({processImport: false}))
-//         .pipe(gulp.dest(F_destCssMinDir))
-//         .pipe(notify("front:compileSass was compiled!"));
-// });
-//
-// // Очистка перед новой записью
-// gulp.task('front:clean', function () {
-//     return gulp.src([F_destCssDir, F_destCssMinDir], {read: false})
-//         .pipe(clean())
-//         .pipe(notify("front:clean was compiled!"));
-// });
-//
-// //------------------------------------------------------------------------------
-// //Наблюдение за файлами. (запуск из консоли - gulp watch)
-// //------------------------------------------------------------------------------
-//
-// gulp.task('watch', function () {
-//
-//     gulp.watch(F_sassDir + '**/*.scss', [
-//         'front:clean',
-//         'front:compileSass',
-// //        'front:combineMq'
-//     ]);
-//
-// });
-
-gulp.task('hello', function () {
-    console.log('**/*.scss');
+gulp.task('build', function () {
+    gulp.series('clean', gulp.parallel('img', 'sass', 'scripts'))
 });
+
